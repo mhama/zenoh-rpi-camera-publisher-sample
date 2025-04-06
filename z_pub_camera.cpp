0d3244c94d04bc43704c76762b67fee0081899f3
Makoto Hamanaka <ham.lua@gmail.com>
2025-04-06 16:19:41 +0900

    stereo camera support

--- z_pub_camera.cpp
+++ z_pub_camera.cpp
@@ -20,23 +20,39 @@ int _main(int argc, char **argv) {
     auto config = Config::create_default();
 
     // 送信先のキーを取得
-    auto keyexpr = default_keyexpr;
-
-    // Zenoh セッションを開く
-    std::cout << "Opening Zenoh session..." << std::endl;
-    auto session = Session::open(std::move(config));
-
-    // Publisher を宣言
-    std::cout << "Declaring Publisher on '" << keyexpr << "'..." << std::endl;
-    auto pub = session.declare_publisher(KeyExpr(keyexpr));
+    std::string keyexpr = default_keyexpr;
 
     // libcamerasrcからだと流し込める。
     // camera-name は libcamera-hello --list-camera の結果の括弧 () 内の文字列
     // 0: "/base/axi/pcie@120000/rp1/i2c@88000/imx708@1a"
     // 1: "/base/axi/pcie@120000/rp1/i2c@80000/imx708@1a"
+    std::string camera_name_l = "/base/axi/pcie@120000/rp1/i2c@88000/imx708@1a";
+    std::string camera_name_r = "/base/axi/pcie@120000/rp1/i2c@80000/imx708@1a";
+    std::string camera_name = camera_name_l;
+    if (argc >= 2) {
+	std::cout << "param:" << argv[1] << std::endl;
+	if (strcmp(argv[1], "L") == 0) {
+	    keyexpr = std::string(default_keyexpr) + "/left"; 
+	    camera_name = camera_name_l;
+	}
+	if (strcmp(argv[1], "R") == 0) {
+	    keyexpr = std::string(default_keyexpr) + "/right"; 
+	    camera_name = camera_name_r;
+	}
+    }
+
+
+    // gstreamer pipeline
+    // options of libcamerasrc can be inspected with below command on the shell:
+    // gst-inspect-1.0 libcamerasrc
+    // af-mode=2 means continuous autofocus
     std::string pipeline =
-        "libcamerasrc camera-name=\"/base/axi/pcie@120000/rp1/i2c@88000/imx708@1a\" "
-	"! video/x-raw,format=RGBx,width=640,height=480,framerate=30/1 "
+        "libcamerasrc af-mode=2 ae-exposure-mode=1 exposure-value=-0.5 camera-name=\"" + camera_name + "\" "
+	"! video/x-raw,format=RGBx,width=960,height=540,framerate=20/1 "
+	//"! video/x-raw,format=RGBx,width=1280,height=720,framerate=10/1 "
+	//"! video/x-raw,format=RGBx,width=2304,height=1296,framerate=10/1 " // for full fov of v3 camera
+	//"! videoscale "
+	//"! video/x-raw,format=RGBx,width=1152,height=648 "
         "! videoconvert "
         "! appsink";
 
@@ -51,6 +67,15 @@ int _main(int argc, char **argv) {
         return -1;
     }
 
+    // Zenoh セッションを開く
+    std::cout << "Opening Zenoh session..." << std::endl;
+    auto session = Session::open(std::move(config));
+
+    // Publisher を宣言
+    std::cout << "Declaring Publisher on '" << keyexpr << "'..." << std::endl;
+    auto pub = session.declare_publisher(KeyExpr(keyexpr));
+
+
     const int delay_ms = 1;
 
     std::cout << "Start capturing and publishing..." << std::endl;
@@ -59,7 +84,7 @@ int _main(int argc, char **argv) {
 
     std::vector<int> param = std::vector<int>(2);
     param[0]=cv::IMWRITE_JPEG_QUALITY;
-    param[1]=85;
+    param[1]=75;
 
     // メインループ
     while (true) {
@@ -77,7 +102,7 @@ int _main(int argc, char **argv) {
             continue;
         }
 
-        // バイト列を文字列として送信
+        // バイト列を送信
         Bytes payload = std::move(encoded);
 
         // Zenoh で送信
